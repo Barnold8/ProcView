b81dd0a6b28f27c4c1d4fe023c5737ade56674a2
Barnold88 <bandslover@outlook.com>
2024-11-08 01:43:47 +0000

    Add some basic code to get proc list

--- app/src/main.go
+++ app/src/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 )
 
 func main() {
 
-	fmt.Println("Hello world!")
+	cmd := exec.Command("wmic process get Caption, CreationDate")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error running tasklist command: %v", err)
+	}
+	for _, element := range output {
+		fmt.Printf("Running processes:\n%s", element)
+	}
 
 }
