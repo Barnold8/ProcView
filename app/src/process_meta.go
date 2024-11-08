68fb7c7f7a4d7c04160321fba95c32c5059406bc
Barnold88 <bandslover@outlook.com>
2024-11-08 01:25:20 +0000

    Provide a function boilerplate for ParseProcess

--- app/src/process_meta.go
+++ app/src/process_meta.go
@@ -1,9 +1,32 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Process struct {
 	name       string
 	PID        int
-	time_alive time.Time // if theres a time struct, use this
+	time_alive time.Duration // if theres a time struct, use this
+}
+
+func ParseProcesses(str string) []Process {
+
+	var processes []Process
+
+	duration, err := time.ParseDuration("2h30m")
+
+	processes = append(processes, Process{
+		name:       "example_process",
+		PID:        1234,
+		time_alive: duration,
+	})
+
+	if err != nil {
+		fmt.Println("Error parsing duration:", err)
+		return nil
+	}
+
+	return processes
 }
