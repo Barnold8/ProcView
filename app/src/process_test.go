d5162c8d46184de81f5aebc6de4f9d61fe06a1d1
Barnold88 <bandslover@outlook.com>
2024-11-11 13:55:39 +0000

    Add test case and testing methodology for process gather

--- app/src/process_test.go
+++ app/src/process_test.go
@@ -1,37 +1,53 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestGatherProcesses(t *testing.T) {
 
-	duration, err := time.ParseDuration("2h30m")
-
+	// PROCESS SETS
 	process_set1 := []Process{
 
 		Process{
-			name:       "f",
-			PID:        1234,
-			time_alive: duration,
+			name:       "test.exe",
+			time_start: time.Date(2024, 11, 11, 13, 4, 54, 0, time.UTC),
+			time_alive: time.Duration(0),
 		},
 	}
+	// PROCESS SETS
 
+	// TESTS
 	tests := []struct {
 		name        string
 		proc_string string
 		expected    []Process
 	}{
-		{"Correct data", "fffff", process_set1}, // this is not actually correct yet
+		{"Correct data", "test.exe                         20241111130454.507396+000", process_set1},
 	}
+	// TESTS
 
+	// Actual testing
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			result := ParseProcesses(tc.proc_string)
 			if result != nil {
 
+				for i, expectedProc := range tc.expected {
+
+					if result[i].name != expectedProc.name {
+						t.Errorf("Expected name %s, got %s", expectedProc.name, result[i].name)
+					}
+					if !reflect.DeepEqual(result[i].time_alive, expectedProc.time_alive) {
+						t.Errorf("Expected time_alive %v, got %v", expectedProc.time_alive, result[i].time_alive)
+					}
+
+				}
+
 			}
 		})
 	}
+	// Actual testing
 }
