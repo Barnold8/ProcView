f21934851a0aaf3ecd96a99eaff29034bf6e6e1a
Barnold88 <bandslover@outlook.com>
2024-11-14 21:57:42 +0000

    Add basic builder pattern for window thus far

---
+++ app/src/windowBuilder.go
@@ -0,0 +1,24 @@
+package main
+
+type IWindowBuilder interface {
+	SetWindowPosition(x float32, y float32) IWindowBuilder
+	SetWindowTitle(title string) IWindowBuilder
+}
+
+type ConcreteWindowBuilder struct {
+	processWindow procWindow
+}
+
+func (w *ConcreteWindowBuilder) SetWindowPosition(x float32, y float32) IWindowBuilder {
+	w.processWindow.x = x
+
+	w.processWindow.window.Content().Position().AddXY(x, y)
+	return w
+}
+
+func (w *ConcreteWindowBuilder) SetWindowTitle(title string) IWindowBuilder {
+	w.processWindow.window.SetTitle(title)
+	return w
+}
+
+func (w *ConcreteWindowBuilder) Build() procWindow { return w.processWindow }
