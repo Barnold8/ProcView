dcc94ba69b9a3d018e0704736847416a57e7f607
Barnold88 <bandslover@outlook.com>
2024-11-14 22:33:40 +0000

    Add bool to check if window is allocated in memory

--- app/src/window.go
+++ app/src/window.go
@@ -4,12 +4,13 @@ import (
 	"fyne.io/fyne/v2"
 )
 
-type procWindow struct {
+type ProcWindow struct {
 	x         float32
 	y         float32
 	width     float32
 	height    float32
 	dark_mode bool
+	is_init   bool
 	app       fyne.App
 	window    fyne.Window
 }
