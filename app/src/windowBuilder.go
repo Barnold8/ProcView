0b1f84f6bf3dea7be92dba0ed7191a11c529a689
Barnold88 <bandslover@outlook.com>
2024-11-14 22:51:03 +0000

    Add a way to error check if window is initialised in memory and force fatal error in possible crash state

--- app/src/windowBuilder.go
+++ app/src/windowBuilder.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"log"
+
+	"fyne.io/fyne/v2/app"
+)
+
 type IWindowBuilder interface {
+	InitialiseWindow() IWindowBuilder
 	SetWindowPosition(x float32, y float32) IWindowBuilder
 	SetWindowTitle(title string) IWindowBuilder
 	Build() ProcWindow
@@ -10,16 +17,33 @@ type ConcreteWindowBuilder struct {
 	processWindow ProcWindow
 }
 
-func (w *ConcreteWindowBuilder) SetWindowPosition(x float32, y float32) IWindowBuilder {
-	w.processWindow.x = x
+func (w *ConcreteWindowBuilder) InitialiseWindow() IWindowBuilder {
+	w.processWindow.app = app.New()
+	w.processWindow.window = w.processWindow.app.NewWindow("")
+	w.processWindow.is_init = true
+	return w
+}
 
+func (w *ConcreteWindowBuilder) CheckInit() {
+	if w.processWindow.is_init == false {
+		log.Fatal("Window is not initialised in memory, run InitialiseWindow() first")
+	}
+}
+
+func (w *ConcreteWindowBuilder) SetWindowPosition(x float32, y float32) IWindowBuilder {
+	w.CheckInit()
+	w.processWindow.x = x
 	w.processWindow.window.Content().Position().AddXY(x, y)
 	return w
 }
 
 func (w *ConcreteWindowBuilder) SetWindowTitle(title string) IWindowBuilder {
+	w.CheckInit()
 	w.processWindow.window.SetTitle(title)
 	return w
 }
 
-func (w *ConcreteWindowBuilder) Build() ProcWindow { return w.processWindow }
+func (w *ConcreteWindowBuilder) Build() ProcWindow {
+	w.CheckInit()
+	return w.processWindow
+}
