335a68d6af95ff2419527e2070f0b133746998f3
Barnold88 <bandslover@outlook.com>
2024-11-15 18:12:29 +0000

    Add background elements to procwindow type

--- app/src/window.go
+++ app/src/window.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 )
 
 type ProcWindow struct {
-	x         float32
-	y         float32
-	width     float32
-	height    float32
-	dark_mode bool
-	is_init   bool
-	app       fyne.App
-	window    fyne.Window
-	data      []string // holds the list data
+	x                    float32
+	y                    float32
+	width                float32
+	height               float32
+	dark_mode            bool
+	is_init              bool
+	app                  fyne.App
+	window               fyne.Window
+	data                 []string // holds the list data
+	background_color     color.NRGBA
+	background_container *fyne.Container
+	window_contents      *fyne.Container
 }
