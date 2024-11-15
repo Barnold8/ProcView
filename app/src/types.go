23668a41da7f42158bb301671ca4ac1ef19a469b
Barnold88 <bandslover@outlook.com>
2024-11-15 19:49:37 +0000

    Force pair to be a float32 pair due to type assertion within fyne function on resizer

--- app/src/types.go
+++ app/src/types.go
@@ -2,6 +2,28 @@ package main
 
 // This file is for rudimentary types I want to make to help me along
 
-type Pair struct {
-	a, b interface{}
+type Float32Pair struct {
+	a, b float32
+}
+
+func ValidateDimensions(width, height float32) Float32Pair { // all this because go has no ternary
+
+	var temp_width float32
+	var temp_height float32
+
+	if width <= 0 {
+		temp_width = 100
+	} else {
+		temp_width = width
+	}
+
+	if height <= 0 {
+		temp_height = 100
+	} else {
+		temp_height = height
+	}
+
+	float_pair := Float32Pair{temp_width, temp_height}
+
+	return float_pair
 }
