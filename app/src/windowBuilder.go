7384fc84bc5baa2a201fa8714d01a3942f81c1a1
Barnold88 <bandslover@outlook.com>
2024-11-15 19:14:11 +0000

    Add window size option, start data contents (data binding) builder function

--- app/src/windowBuilder.go
+++ app/src/windowBuilder.go
@@ -12,6 +12,8 @@ type IWindowBuilder interface {
 	SetWindowPosition(x float32, y float32) IWindowBuilder
 	SetWindowTitle(title string) IWindowBuilder
 	SetWindowContainer(container *fyne.Container) IWindowBuilder
+	SetWindowSize(width float32, height float32) IWindowBuilder
+	SetDataContents(string_ptr *[]string) IWindowBuilder
 	Build() ProcWindow
 }
 
@@ -39,9 +41,23 @@ func (w *ConcreteWindowBuilder) SetWindowPosition(x float32, y float32) IWindowB
 	return w
 }
 
+func (w *ConcreteWindowBuilder) SetWindowSize(width float32, height float32) IWindowBuilder {
+	w.CheckInit()
+
+	w.processWindow.width = width
+	w.processWindow.height = height
+
+	return w
+}
+
 func (w *ConcreteWindowBuilder) SetWindowTitle(title string) IWindowBuilder {
 	w.CheckInit()
 	w.processWindow.window.SetTitle(title)
+	return w
+}
+
+func (w *ConcreteWindowBuilder) SetDataContents(string_ptr *[]string) IWindowBuilder {
+
 	return w
 }
 
@@ -53,6 +69,24 @@ func (w *ConcreteWindowBuilder) SetWindowContainer(container *fyne.Container) IW
 
 func (w *ConcreteWindowBuilder) Build() ProcWindow {
 	w.CheckInit()
+
+	var width float32
+	var height float32
+
+	if w.processWindow.width <= 0 {
+		width = 100
+	} else {
+		width = w.processWindow.width
+	}
+
+	if w.processWindow.height <= 0 {
+		height = 100
+	} else {
+		height = w.processWindow.height
+	}
+
+	w.processWindow.window.Resize(fyne.NewSize(width, height))
+
 	if w.processWindow.window_contents != nil {
 		w.processWindow.window.SetContent(w.processWindow.window_contents)
 	}
