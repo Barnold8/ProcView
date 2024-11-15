0406da3695358d084b981a5c5ebcde999ce7468a
Barnold88 <bandslover@outlook.com>
2024-11-15 19:37:24 +0000

    Remove background colour option, customisation of fyne components not as robust as once thought

--- app/src/window.go
+++ app/src/window.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/data/binding"
 )
 
 type ProcWindow struct {
-	x                float32
-	y                float32
-	width            float32
-	height           float32
-	app              fyne.App
-	window           fyne.Window
-	data             binding.ExternalStringList
-	background_color color.NRGBA
-	window_contents  *fyne.Container
+	x               float32
+	y               float32
+	width           float32
+	height          float32
+	app             fyne.App
+	window          fyne.Window
+	window_contents *fyne.Container
 }
