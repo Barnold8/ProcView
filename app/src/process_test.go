c1cc17d6fd18df8bc1b29f7402b75b13e64a047d
Barnold88 <bandslover@outlook.com>
2024-11-17 16:45:54 +0000

    Remove duplicate test has been removed and body of parse processes test has been removed

--- app/src/process_test.go
+++ app/src/process_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 	"time"
 )
@@ -37,7 +36,7 @@ func TestTimeParser(t *testing.T) { // TODO: add fail cases where the function h
 
 }
 
-func TestGatherProcesses(t *testing.T) {
+func TestGatherProcesses(t *testing.T) { // TODO: change this to reflect hashmap
 
 	// PROCESS SETS
 	process_set1 := []Process{ //TODO: add more test cases
@@ -62,71 +61,10 @@ func TestGatherProcesses(t *testing.T) {
 	// Actual testing
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			result := ParseProcesses(tc.proc_string)
-			if result != nil {
 
-				for i, expectedProc := range tc.expected { // check against the returned list of processes to the expected ones and check their attributes
-
-					if result[i].name != expectedProc.name {
-						t.Errorf("Expected name %s, got %s", expectedProc.name, result[i].name)
-					}
-					if !reflect.DeepEqual(result[i].time_alive, expectedProc.time_alive) {
-						t.Errorf("Expected time_alive %v, got %v", expectedProc.time_alive, result[i].time_alive)
-					}
-
-				}
-
-			} else {
-				t.Errorf("Expected []Process but got nil")
-			}
 		})
 	}
 	// Actual testing
-}
-
-func TestRemoveDuplicates(t *testing.T) {
-
-	process_duplicates1 := []Process{
-		Process{
-			name:       "test.exe",
-			time_start: time.Date(2024, 11, 11, 13, 4, 54, 0, time.UTC),
-			time_alive: time.Duration(0),
-		},
-		Process{
-			name:       "test.exe",
-			time_start: time.Date(2024, 11, 11, 13, 4, 54, 0, time.UTC),
-			time_alive: time.Duration(0),
-		},
-	}
-
-	process_duplicates1_sorted := []Process{
-		Process{
-			name:       "test.exe",
-			time_start: time.Date(2024, 11, 11, 13, 4, 54, 0, time.UTC),
-			time_alive: time.Duration(0),
-		},
-	}
-
-	// TESTS
-	tests := []struct {
-		name     string
-		unsorted []Process
-		expected []Process
-	}{
-		{"Duplicates test 1", process_duplicates1, process_duplicates1_sorted},
-	}
-	// TESTS
-
-	for _, tc := range tests {
-
-		result := removeDuplicateProcesses(tc.unsorted)
-
-		if !(reflect.DeepEqual(tc.expected, result)) {
-			t.Errorf("sortProcess function did not correctly remove duplicates (removing duplicates)")
-		}
-
-	}
-
 }
 
 // if i ever want to make a function to sort the processes, make a test here
