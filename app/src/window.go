71c54677562cfb7622357930d9d9b3aa0143bee1
Barnold88 <bandslover@outlook.com>
2024-11-20 23:50:17 +0000

    Remove unused fields of ProcWindow type

--- app/src/window.go
+++ app/src/window.go
@@ -10,16 +10,13 @@ import (
 )
 
 type ProcWindow struct {
-	x                      float32
-	y                      float32
-	width                  float32
-	height                 float32
-	app                    fyne.App
-	window                 fyne.Window
-	window_contents        *fyne.Container
-	background_process     FuncPointerNoArgs
-	background_process_one FuncPointerOneArgs
-	background_process_two FuncPointerTwoArgs
+	x               float32
+	y               float32
+	width           float32
+	height          float32
+	app             fyne.App
+	window          fyne.Window
+	window_contents *fyne.Container
 }
 
 func CreateBox() *fyne.Container {
