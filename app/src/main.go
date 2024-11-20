e01c1d0227159433d2d4f981d3820e58d3692219
Barnold88 <bandslover@outlook.com>
2024-11-20 23:51:41 +0000

    A lot of stuff...

--- app/src/main.go
+++ app/src/main.go
@@ -2,22 +2,91 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
+	"image/color"
+	"strings"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
 )
+
+func appendData(previous_data binding.ExternalStringList, processes map[string]Process) {
+
+	for {
+
+		processData := ProcessMapToString(UpdateProcesses(processes, time.Now(), string(grabProcesses())))
+		fmt.Println(processData)
+
+		err := previous_data.Set(append([]string{"Name, Start, Time"}, strings.Split(processData, "\n")...))
+
+		if err != nil {
+
+			panic(err)
+		}
+	}
+}
 
 func main() {
 
-	cmd := exec.Command("wmic.exe", "process", "get", "Caption,CreationDate")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("Error running tasklist command: %v", err)
-	}
+	myFunc := appendData
 
-	unsorted := ParseProcesses(string(output))
+	data := binding.BindStringList(
+		&[]string{ProcessMapToString(ParseProcesses(string(grabProcesses())))},
+	)
 
-	sorted := removeDuplicateProcesses(unsorted)
+	go myFunc(data, ParseProcesses(string(grabProcesses())))
 
-	fmt.Println(sorted)
+	// GUI MESS, NOT SURE HOW TO MAKE CLEAN
+	list := widget.NewListWithData(data,
+		func() fyne.CanvasObject {
+			// Create a row with dynamically sized boxes
+			return container.NewGridWithColumns(3, // Adjust column count as necessary
+				CreateBox(), CreateBox(), CreateBox(),
+			)
+		},
+		func(i binding.DataItem, o fyne.CanvasObject) {
+			// Bind data to each box
+			grid := o.(*fyne.Container)
+			stringValue, err := i.(binding.String).Get()
+			if err != nil {
+				return
+			}
+
+			labels := strings.Split(stringValue, ",")
+			for idx, child := range grid.Objects {
+				if idx < len(labels) {
+					box := child.(*fyne.Container)
+					label := box.Objects[1].(*widget.Label)
+					label.SetText(labels[idx])
+				}
+			}
+		})
+
+	// // Create a rectangle for the background
+	background := canvas.NewRectangle(color.NRGBA{R: 200, G: 200, B: 255, A: 255}) // Light blue background
+
+	// // Create layout content
+	title := widget.NewLabel("Process Viewer")
+	title.Alignment = fyne.TextAlignCenter
+
+	// // Use a vertical box layout for the content
+	content := container.NewBorder(title, nil, nil, nil, list)
+
+	// // Combine background and content using container.NewMax
+	mainContent := container.NewStack(
+		background,
+		content,
+	)
+
+	// GUI MESS, NOT SURE HOW TO MAKE CLEAN
+
+	windowBuilder := ConcreteWindowBuilder{}
+
+	pWindow := windowBuilder.InitialiseWindow().SetWindowContainer(mainContent).SetWindowSize(900, 500).Build()
+
+	pWindow.window.ShowAndRun()
 
 }
