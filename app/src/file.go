3446f884960e399ad178a5d4e358fab81468d426
Barnold88 <bandslover@outlook.com>
2024-11-25 20:52:49 +0000

    Add file saving capability

--- app/src/file.go
+++ app/src/file.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func ProcessMapToCSV() string {
 
 	return ""
 }
 
-func SaveToFile()
+func SaveToFile(filename string, contents string) {
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	// Write data to the file
+	_, err = file.WriteString(contents)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	file.Close()
+
+}
