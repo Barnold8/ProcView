14057917565c5b26238a6ee19d6797d3dd89c7e6
Barnold88 <bandslover@outlook.com>
2024-11-25 20:58:27 +0000

    Add processmap to csv function

--- app/src/file.go
+++ app/src/file.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func ProcessMapToCSV() string {
+func ProcessMapToCSV(processes map[string]Process) string {
 
-	return ""
+	var builder strings.Builder
+
+	builder.WriteString("Process Name, Time Created, RunTime\n")
+
+	for _, value := range processes {
+		builder.WriteString(fmt.Sprintf("%s,%s,%s", value.name, value.time_start, value.time_alive))
+	}
+
+	return builder.String()
 }
 
 func SaveToFile(filename string, contents string) {
