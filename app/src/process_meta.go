1839294b531b4f2c3895cff11bd62b7d10a8dfde
Barnold88 <bandslover@outlook.com>
2024-11-25 22:56:43 +0000

    Add formatting functions to reformat strings before they are displayed in the UI

--- app/src/process_meta.go
+++ app/src/process_meta.go
@@ -66,6 +66,58 @@ func parseTime(timeStr string) (time.Time, error) {
 	return process_time, nil
 }
 
+func reformatDate(input string) (string, error) {
+
+	layout := "2006-01-02 15:04:05 -0700 UTC"
+
+	t, err := time.Parse(layout, input)
+	if err != nil {
+		return "", err
+	}
+
+	return t.Format("2006-01-02 15:04:05"), nil
+}
+
+func reformatDuration(input string) string {
+
+	re := regexp.MustCompile(`(\d+(\.\d+)?)s`)
+	input = re.ReplaceAllStringFunc(input, func(s string) string {
+
+		parts := strings.Split(s, ".")
+		return parts[0] + "s"
+	})
+
+	duration, err := time.ParseDuration(input)
+	if err != nil {
+		return "Invalid time format"
+	}
+
+	days := int(duration.Hours()) / 24
+	hours := int(duration.Hours()) % 24
+	minutes := int(duration.Minutes()) % 60
+	seconds := int(duration.Seconds()) % 60
+
+	var result []string
+	if days > 0 {
+		result = append(result, fmt.Sprintf("%d days", days))
+	}
+	if hours > 0 {
+		result = append(result, fmt.Sprintf("%d hours", hours))
+	}
+	if minutes > 0 {
+		result = append(result, fmt.Sprintf("%d minutes", minutes))
+	}
+	if seconds > 0 {
+		result = append(result, fmt.Sprintf("%d seconds", seconds))
+	}
+
+	if len(result) == 0 {
+		result = append(result, "0 seconds")
+	}
+
+	return strings.Join(result, " ")
+}
+
 func ParseProcesses(str string) map[string]Process {
 
 	var split []string = strings.Split(str, "\n")
@@ -147,7 +199,17 @@ func ProcessMapToStringSortedByName(processes map[string]Process, inverse bool) 
 
 	if inverse {
 		for i := len(keys) - 1; i >= 0; i-- {
-			builder.WriteString(fmt.Sprintf("%s, %s, %s,\n", processes[keys[i]].name, processes[keys[i]].time_start, processes[keys[i]].time_alive))
+
+			timeStart, timeErr := reformatDate(processes[keys[i]].time_start.String())
+			timeAlive := reformatDuration(processes[keys[i]].time_alive.String())
+
+			if timeErr != nil {
+				timeStart = "Error!"
+			}
+
+			fmt.Println(timeStart, timeAlive)
+
+			builder.WriteString(fmt.Sprintf("%s, %s, %s,\n", processes[keys[i]].name, timeStart, timeAlive))
 		}
 
 	} else {
@@ -180,6 +242,13 @@ func ProcessMapToStringSortedByTimeStarted(processes map[string]Process, inverse
 	}
 
 	return builder.String()
+}
+
+func formatData(data string) string {
+
+	// fmt.Println(data)
+
+	return ""
 }
 
 func ProcessMapToStringSortedByTimeAlive(processes map[string]Process, inverse bool) string {
